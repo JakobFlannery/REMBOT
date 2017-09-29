e8ffffdc05980250eac8b0baf314725d7cb196b9
JakobFlannery <32347696+JakobFlannery@users.noreply.github.com>
2017-09-29 23:34:21 +1000

    Second Version
    Added more 'sensitive' Rem

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
+const words = require('./wordbank.json')
 require('./util/eventLoader')(client)
 
 client.on("message", message => {
-    if (message.content === "Rem is bad"){
-        message.reply('LIES!')
+    for (var name = 0; i < (words.rem).length; name++) {
+        for (var word = 0; i < (words.bad).length; word++) {
+            insult = `${name} is ${word}`
+            if ((message.content).toLowerCase() === insult){
+                message.reply('LIES!')
+        }
+    }
     }
 })
 
